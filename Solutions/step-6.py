560ede69198830f5295e6d9bbc73d3f7e07ea2ab
Khushal Agrawal <agrawalkhushal04@gmail.com>
2023-10-20 17:05:43 +0530

    Fixes

--- Solutions/step-6.py
+++ Solutions/step-6.py
@@ -6,7 +6,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Brick Breaker")
+pygame.display.set_caption("Reflex Racers")
 
 FPS = 60
 VEL = 7
